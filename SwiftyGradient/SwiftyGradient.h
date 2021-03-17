530d78759c798608dfba4c16b7bafc980a72f271
Syed Rafay <syed.rafay@invisionsolutions.com>
2021-03-17 13:01:19 +0500

    first Commit

---
+++ SwiftyGradient/SwiftyGradient.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyGradient.h
+//  SwiftyGradient
+//
+//  Created by Invision on 17/03/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyGradient.
+FOUNDATION_EXPORT double SwiftyGradientVersionNumber;
+
+//! Project version string for SwiftyGradient.
+FOUNDATION_EXPORT const unsigned char SwiftyGradientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyGradient/PublicHeader.h>
+
+
